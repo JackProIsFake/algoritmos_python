6d1b437fafeaf89b08f6be03f584a3a8fd81870c
JackProIsFake <downloadgames310806@gmail.com>
2024-06-26 12:32:20 -0300

    Aula2

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ('hello')
+print('########################################')
+print('#         ALGORITMOS EM PYTHON         #')
+print('########################################')
+print('# Escolha a opção desejada             #')
+print('#                                      #')
+print('# 1. Fibonacci                         #')
+print('########################################')
+
+try:
+    choice = int(input())
+
+    if choice == 1:
+       print('FIB')
+    else:
+        print('Opção invalida')
+except:
+    print('Ocorreu um erro...')
