393e57e0e6c3beacba71a6ab8c3b85481fbd260f
JackProIsFake <downloadgames310806@gmail.com>
2024-06-28 12:21:28 -0300

    Aula 3

--- scripts/fibonacci.py
+++ scripts/fibonacci.py
@@ -1,5 +1,11 @@
 # Retorna os X primeiros números da sequência
 def fibonacci(qtt:int):
     sequence = [0,1]
+
+    i = 2
+    
+    while i < 10:
+        sequence.append(sequence[i-1] + sequence[i-2])
+        i +=1
     
     return sequence
